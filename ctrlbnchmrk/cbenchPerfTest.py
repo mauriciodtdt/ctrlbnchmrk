0a2e512e4a25cb39332d3b617e5c517dbee4517d
mauriciodtdt <mauriciodtdt@hotmail.com>
2018-09-17 16:27:54 +1000

    Cleanup

--- ctrlbnchmrk/cbenchPerfTest.py
+++ ctrlbnchmrk/cbenchPerfTest.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
-
-from cbenchConf import *
+#from cbenchConf import *
 import subprocess
 import logging
 import re
@@ -8,46 +7,76 @@ import json
 import sys
 from logging.handlers import TimedRotatingFileHandler
 
-################# 
-#Functions to log results and errors
-###############
+sys.path.append('../etc/')
+import config
 
-FORMATTER = logging.Formatter("%(asctime)s;%(name)s;%(levelname)s;%(message)s")
-LOG_FILE = "cbenchPerfTest.log"
+CONTROLLER_IP=config.CBENCH_CONFIG['CONTROLLER_IP']
+CONTROLLER_PORT=config.CBENCH_CONFIG['CONTROLLER_PORT']
+MS_PER_TEST=config.CBENCH_CONFIG['MS_PER_TEST']
+TESTS_PER_SWITCH=config.CBENCH_CONFIG['TESTS_PER_SWITCH']
+NUM_SWITCHES=config.CBENCH_CONFIG['NUM_SWITCHES']
+NUM_MACS=config.CBENCH_CONFIG['NUM_MACS']
+CBENCH_WARMUP=config.CBENCH_CONFIG['CBENCH_WARMUP']
+TEST_LOOP=config.CBENCH_CONFIG['TEST_LOOP']
 
-def get_console_handler():
-  console_handler = logging.StreamHandler()
-  console_handler.setFormatter(FORMATTER)
-  return console_handler
-def get_file_handler():
-  file_handler = TimedRotatingFileHandler(LOG_FILE, when='midnight')
-  file_handler.setFormatter(FORMATTER)
-  return file_handler
-def get_logger(logger_name):
-  logger = logging.getLogger(logger_name)
-  logger.setLevel(logging.DEBUG) # better to have too much log than not enough
-  logger.addHandler(get_console_handler())
-  logger.addHandler(get_file_handler())
-  logger.propagate = False # with this pattern, it's rarely necessary to propagate the error up to parent
-  return logger
+DATA_PATH='../data/'
 
-my_logger = get_logger("CBENCH")
+###########################################################
+#Functions to log results and errors in csv and json format
+###########################################################
 
-#################
-# Cbench run with parameters imported from cbenchConf file
-################
+FORMATTER_CSV = logging.Formatter("%(asctime)s;%(name)s;%(message)s")
+LOG_FILE_CSV = "DATA_PATH/cbenchPerfTest.log"
+
+FORMATTER_JSON = logging.Formatter("{\"Test\": %(name)s, \"Message\": {\"Time\": %(asctime)s, \"Measures\": [%(message)s]}}")
+LOG_FILE_JSON = "DATA_PATCH/cbenchPerfTest.json"
+
+def get_console_handler(f):
+   console_handler = logging.StreamHandler()
+   if (f == 'csv'):
+      console_handler.setFormatter(FORMATTER_CSV)
+#      print "formater csv"
+   else:
+      console_handler.setFormatter(FORMATTER_JSON)
+#      print "formater json"
+   return console_handler
+
+def get_file_handler(f):
+   if (f == 'csv'):
+      file_handler = TimedRotatingFileHandler(LOG_FILE_CSV, when='midnight')
+      file_handler.setFormatter(FORMATTER_CSV)
+   else:
+      file_handler = TimedRotatingFileHandler(LOG_FILE_JSON, when='midnight')
+      file_handler.setFormatter(FORMATTER_JSON)
+   return file_handler
+
+def get_logger(logger_name,ff):
+   logger = logging.getLogger(logger_name)
+   logger.setLevel(logging.DEBUG) # better to have too much log than not enough
+   logger.addHandler(get_console_handler(ff))
+   logger.addHandler(get_file_handler(ff))
+   logger.propagate = False # with this pattern, it's rarely necessary to propagate the error up to parent
+   return logger
+ 
+my_logger_csv = get_logger("CBENCHCSV",'csv')
+#print my_logger_csv
+my_logger_json = get_logger("CBENCHJSON",'json')
+  
+ ##########################################################
+ # Cbench run with parameters imported from cbenchConf file
+ ##########################################################
 i=0
-while (i<LOOP):
-  cmdcbench = "cbench -c %s -p %s -m %s -l %s -s %s -M %s -w %s -t" % (CONTROLLER_IP, CONTROLLER_PORT, MS_PER_TEST, TESTS_PER_SWITCH, NUM_SWITCHES, NUM_MACS, CBENCH_WARMUP) 
-  cbench=subprocess.Popen(cmdcbench, stdout=subprocess.PIPE, shell=True)
-  (output, err) = cbench.communicate()
-  cbenchResults = output[output.index("RESULT"):]
-  cbenchResults = cbenchResults.replace("\n", " ")
-  switches = cbenchResults.split()[1]
-  macs = NUM_MACS
-  avg_num_flows = cbenchResults.split()[7].split('/')[2]
-  json_results = {}
-  results = "%s;%s;%s" % (switches,macs,avg_num_flows)
-  my_logger.debug(results)
-
-  i+=1
+while (i<TEST_LOOP):
+   cmdcbench = "cbench -c %s -p %s -m %s -l %s -s %s -M %s -w %s -t" % (CONTROLLER_IP, CONTROLLER_PORT, MS_PER_TEST, TESTS_PER_SWITCH, NUM_SWITCHES, NUM_MACS, CBENCH_WARMUP)
+   cbench=subprocess.Popen(cmdcbench, stdout=subprocess.PIPE, shell=True)
+   (output, err) = cbench.communicate()
+   cbenchResults = output[output.index("RESULT"):]
+   cbenchResults = cbenchResults.replace("\n", " ")
+   switches = cbenchResults.split()[1]
+   macs = NUM_MACS
+   avg_num_flows = cbenchResults.split()[7].split('/')[2]
+   results_csv = "%s;%s;%s" % (switches,macs,avg_num_flows)
+   results_json = "{switches: %s, macs: %s, avg_num_flows: %s}" % (switches,macs,avg_num_flows)
+   my_logger_csv.debug(results_csv)
+   my_logger_json.debug(results_json)
+   i+=1
---
+++ etc/config.py
@@ -0,0 +1,15 @@
+# config.py
+# Contains configuration for the testing environment, cbench, mininet
+# and wireshark.
+
+CBENCH_CONFIG = {
+   'NUM_SWITCHES': '5', # Default number of switches for CBench to simulate
+   'NUM_MACS': '100', # Default number of MACs for CBench to use
+   'TESTS_PER_SWITCH': '11', # Default number of CBench tests to do per CBench run
+   'MS_PER_TEST': '1000', # Default milliseconds to run each CBench test
+   'CBENCH_WARMUP': '1', # Default number of warmup cycles to run CBench
+   'CONTROLLER': 'ODL', # Currently only support ODL
+   'CONTROLLER_IP': 'localhost', # Change this to remote IP if running on two systems
+   'CONTROLLER_PORT': '6633', # Default port for OpenDaylight
+   'TEST_LOOP': '20' #Times that CBench will run
+}
