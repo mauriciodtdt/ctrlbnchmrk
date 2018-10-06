ecbebb7dbb55d766e31b5eb6d537b1072aac53b1
mauriciodtdt <mauriciodtdt@hotmail.com>
2018-10-06 20:54:21 +1000

    link_number fixed in logging

--- ctrlbnchmrk/network_topology_discovery_13.py
+++ ctrlbnchmrk/network_topology_discovery_13.py
@@ -93,13 +93,13 @@ def tshark_disect(q):
 #         print sw_linked
       elif "Port Subtype" in line and begin_flag == True and PIN_flag == True:
          port_linked = line.split(":")[1].strip()
-         link_number += 1
 #         print (port_linked)
          sw_link_left = sw_array[tcp_port][0]
          link_info = "%s-%s<-->%s-%s" % (sw_link_left, sw_port_in,sw_linked,port_linked)
-         tshark_logger_csv.debug("%s;%s" % (link_number, link_info))
          if not link_info in link_array:
-            link_array[link_info] = timestamp
+            link_number += 1
+            tshark_logger_csv.debug("%s;%s;%s" % (timestamp, link_number, link_info))
+            link_array[link_info] = (timestamp,link_number)
       elif "OFPT_PACKET_OUT" in line:
          begin_flag = False
       
